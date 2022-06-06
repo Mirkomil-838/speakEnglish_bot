a68e29d3a846e3801f757d1064abf4f9296cc5c8
Mirkomil-838 <85350559+Mirkomil-838@users.noreply.github.com>
2022-06-06 11:10:34 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("5424907566:AAGGuuMSPq05gcrDm3o_1mGYN5NPXw-qDqA")  # Bot toekn
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("213.230.87.164")  # Xosting ip manzili
+IP = env.str("ip")  # Xosting ip manzili
